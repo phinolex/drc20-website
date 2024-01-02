5265d7b13601fabff5ab7d82446d0d1445dc1f3a
phinolex <lukas.jacqueline@hotmail.fr>
2024-01-02 23:28:51 +0100

    update

--- components/home/modal.vue
+++ components/home/modal.vue
@@ -5,5 +5,23 @@
         <n-step title="BOT access"></n-step>
     </n-steps>
 </div>
-test
 </template>
+
+<script setup>
+const currentStep = ref(1);
+const currentTab = ref('1');
+
+const nextStep = () => {
+    if (currentStep.value < 5) {
+        currentStep.value++;
+        currentTab.value = String(currentStep.value);
+    }
+};
+const prevStep = () => {
+    if (currentStep.value > 1) {
+        currentStep.value--;
+        currentTab.value = String(currentStep.value);
+    }
+};
+</script>
+
